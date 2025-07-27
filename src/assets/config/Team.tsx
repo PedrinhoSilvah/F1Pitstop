a5b5415ed9b4cc4bc19084eb49a9b60ff9218e5e
Pedro Miguel <pedrinhosillvah59@gmail.com>
2025-07-27 02:00:51 -0300

    Criando a section Team

---
+++ src/assets/config/Team.tsx
@@ -0,0 +1,26 @@
+import Mercedes from "../imgs/Imgs_Cars/2025mercedescarright-removebg-preview.png";
+import McLaren from "../imgs/Imgs_Cars/2025mclarencarright-removebg-preview.png";
+import Ferrari from "../imgs/Imgs_Cars/2025ferraricarright-removebg-preview.png";
+import Alpine from "../imgs/Imgs_Cars/2025alpinecarright-removebg-preview.png";
+import RedBull from "../imgs/Imgs_Cars/2025racingbullscarright-removebg-preview.png";
+import RedBullRacing from "../imgs/Imgs_Cars/2025redbullracingcarright-removebg-preview.png";
+import AstonMartin from "../imgs/Imgs_Cars/2025astonmartincarright-removebg-preview.png";
+import Williams from "../imgs/Imgs_Cars/2025williamscarright-removebg-preview.png";
+import Hass from "../imgs/Imgs_Cars/2025haascarright-removebg-preview.png";
+import Sauber from "../imgs/Imgs_Cars/2025kicksaubercarright-removebg-preview.png";
+
+const Cars = {
+  Mercedes,
+  McLaren,
+  Ferrari,
+  Alpine,
+  RedBull,
+  RedBullRacing,
+  AstonMartin,
+  Williams,
+  Hass,
+  Sauber,
+};
+
+
+export default Cars
--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,15 +1,124 @@
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import { getEquipes } from "../../services/F1Api";
+import Cars from "../../assets/config/Team";
+import Logos from "../../assets/config/Logos";
+import "./style.scss";
 
 
 const Team = () => {
   return (
     <>
       <Header />
-        <section>
-            <img src="https://img.asmedia.epimg.net/resizer/v2/2AHY4FP6NFHXXNN7RC6OCO7BU4.jpg?auth=9c2f8ea5bdbfe640c8fef82011f6a92d407cb85d2c644710344becba26443bba&width=360&height=203&focal=1882%2C143" alt="" />
-        </section>
+      <section className="Section_Team">
+        <ul className="Ul_Container_Team">
+          <li className="container_Team Mclaren">
+            <img className="Img_Cars" src={Cars.McLaren} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.McLaren} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>Oscar Piastri</h2>
+              <p>|</p>
+              <h2>Lando Norris</h2>
+            </div>
+          </li>
+          <li className="container_Team AstonMartin">
+            <img className="Img_Cars" src={Cars.AstonMartin} alt="Img_Team" />
+            <img
+              className="Logos_Team"
+              src={Logos.AstonMartin}
+              alt="Logo_Team"
+            />
+            <div className="Text_Team">
+              <h2>Lance Stroll</h2>
+              <p>|</p>
+              <h2>Fernando Alonso</h2>
+            </div>
+          </li>
+          <li className="container_Team Hass">
+            <img className="Img_Cars" src={Cars.Hass} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.Hass} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>Esteban Ocon</h2>
+              <p>|</p>
+              <h2>Oliver Bearman</h2>
+            </div>
+          </li>
+          <li className="container_Team Mercedes">
+            <img className="Img_Cars" src={Cars.Mercedes} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.Mercedes} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>George Russell</h2>
+              <p>|</p>
+              <h2>Kimi Antonelli</h2>
+            </div>
+          </li>
+          <li className="container_Team RedBull">
+            <img className="Img_Cars" src={Cars.RedBull} alt="Img_Team" />
+            <img
+              className="Logos_Team"
+              src={Logos.RedBullRacing}
+              alt="Logo_Team"
+            />
+            <div className="Text_Team">
+              <h2>Liam Lawson</h2>
+              <p>|</p>
+              <h2>Isack Hadjar</h2>
+            </div>
+          </li>
+          <li className="container_Team Sauber">
+            <img className="Img_Cars" src={Cars.Sauber} alt="Img_Team" />
+            <img
+              className="Logos_Team_Config"
+              src={Logos.Stake}
+              alt="Logo_Team"
+            />
+            <div className="Text_Team">
+              <h2>Nico Hulkenberg</h2>
+              <p>|</p>
+              <h2>Gabriel Bortoleto</h2>
+            </div>
+          </li>
+          <li className="container_Team Williams">
+            <img className="Img_Cars" src={Cars.Williams} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.Williams} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>Alexander Albon</h2>
+              <p>|</p>
+              <h2>Carlos Sainz</h2>
+            </div>
+          </li>
+          <li className="container_Team RedbullRacing">
+            <img className="Img_Cars" src={Cars.RedBullRacing} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.RedBull} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>Max Verstappen</h2>
+              <p>|</p>
+              <h2>Yuki Tsunoda</h2>
+            </div>
+          </li>
+          <li className="container_Team Ferrari">
+            <img className="Img_Cars" src={Cars.Ferrari} alt="Img_Team" />
+            <img className="Logos_Team" src={Logos.Ferrari} alt="Logo_Team" />
+            <div className="Text_Team">
+              <h2>Charles Leclerc</h2>
+              <p>|</p>
+              <h2>Lewis Hamilton</h2>
+            </div>
+          </li>
+          <li className="container_Team Alpine">
+            <img className="Img_Cars" src={Cars.Alpine} alt="Img_Team" />
+            <img
+              className="Logos_Team_Config"
+              src={Logos.Alpine}
+              alt="Logo_Team"
+            />
+            <div className="Text_Team">
+              <h2>Pierre Gasly</h2>
+              <p>|</p>
+              <h2>Franco Colapinto</h2>
+            </div>
+          </li>
+        </ul>
+      </section>
       <Footer />
     </>
   );
